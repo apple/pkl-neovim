8567580d15bb264e32862b728ab7f1d00ee98568
Dan Chao <dan.chao@apple.com>
2025-05-12 09:01:18 -0700

    Fix validation of timeout_ms
    Function `vim.validate` accepts `"number"`, but not `"integer"`.

--- lua/pkl-neovim/config.lua
+++ lua/pkl-neovim/config.lua
@@ -19,7 +19,7 @@ local M = {}
 
 ---@class pklneovim.Config
 ---@field pkl_cli_path? string
----@field timeout_ms? integer
+---@field timeout_ms? number
 ---@field start_command? string[]
 
 ---@param path string The path to the field being validated
@@ -35,7 +35,7 @@ end
 local function validate(cfg)
   return validate_path("vim.g.pkl_neovim", {
     pkl_cli_path = { cfg.pkl_cli_path, {"string", "nil"} },
-    timeout_ms = { cfg.timeout_ms, {"integer", "nil"} },
+    timeout_ms = { cfg.timeout_ms, {"number", "nil"} },
     start_command = { cfg.start_command, {"table", "nil"} }
   })
 end
